d0dda8f7f104d63ba1fbe5275557ac081b9cafa2
MikeDev0X <mike0vortex@gmail.com>
2023-03-26 19:01:28 -0600

    minor css changes

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,9 @@
 import axios from "axios";
+import login from '../styles/Login.module.css'
 import { use, useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock, faCircleCheck, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 
 export default function Login() {
@@ -7,29 +11,31 @@ export default function Login() {
     const [loginUsername, setLoginUsername] = useState('');
     const [loginPassword, setLoginPassword] = useState('');
 
-    const login = () =>{
+    const loginPost = () =>{
         axios({
             method: ""
 
 
         })
 
-
-
     }
 
-
-
-
     return (
-        <div>
-            <h1>Login</h1>
+        <>
+        <div className={login.grid}>
+            <header className="login.header">
+                <h1>Login</h1>
+            </header>
 
-            <input type="text" name="username" placeholder="username" onChange={e => setLoginUsername(e.target.value)}></input>
-            <input type="password" name="passwrd" placeholder="password" onChange={e => setLoginPassword(e.target.value)}></input>
-            <button>Login</button>
+            <form className={login.form}>
+                <input type="text" name="username" placeholder="username" onChange={e => setLoginUsername(e.target.value)}></input>
+                <input type="password" name="passwrd" placeholder="password" onChange={e => setLoginPassword(e.target.value)}></input>
+                <button>Login</button>
+            </form>
+
 
 
         </div>
+        </>
     )
 }
