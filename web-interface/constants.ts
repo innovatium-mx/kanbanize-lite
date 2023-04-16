79bc0aeed2075a567059e23c9738f0f37bbe55c0
Marco Antonio Gardida Cortés <60368243+Ant20ni02@users.noreply.github.com>
2023-04-15 22:30:40 -0600

    Update urlCloud in constants

--- web-interface/constants.ts
+++ web-interface/constants.ts
@@ -1,2 +1,2 @@
-export const urlCloud : string = 'http://kanbanizelite-env.eba-9kmxm4ki.us-east-1.elasticbeanstalk.com/';
+export const urlCloud : string = 'https://fs96h11zh9.execute-api.us-east-1.amazonaws.com/';
 export const urlLocal : string = 'http://localhost:4000/';
