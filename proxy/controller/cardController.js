80aa04c36b657410c1ce1902e5ca6c523f292f8b
Ant20ni02 <antoniogarcort@hotmail.com>
2023-05-01 10:45:46 -0600

    CardDetails working with co-owners

--- proxy/controller/cardController.js
+++ proxy/controller/cardController.js
@@ -5,6 +5,8 @@ module.exports.cardDetails = async (req,res) =>{
     const cardid = req.params.cardid;
     const apikey = req.headers.apikey;
     var users = [];
+    const co_owner__username = [];
+    const co_owner__avatar = [];
 
     try{
         const response1 = await  fetch(`https://${host}.kanbanize.com/api/v2/cards/${cardid}`, {
@@ -16,6 +18,43 @@ module.exports.cardDetails = async (req,res) =>{
         if (response1.ok){
             const data1 = await response1.json()
             const cardDetails = data1.data;
+            const responseUsers = await  fetch(`https://${host}.kanbanize.com/api/v2/users`, {
+                method: "GET",
+                headers: {
+                    "apikey": apikey
+                },
+            });
+            if(responseUsers.ok){
+                const rawUsers = await responseUsers.json();
+                users = rawUsers.data;
+            }
+            else{
+                res.json({"error": responseUsers.status});
+            }
+            if(cardDetails.owner_user_id){
+                const userObject = users.find(function(item, i){
+                    if(item.user_id === cardDetails.owner_user_id){
+                      index = i;
+                      return i;
+                    }
+                });
+                cardDetails.owner_username = userObject.username;
+                cardDetails.owner_avatar = userObject.avatar;
+            }
+            if(cardDetails.co_owner_ids.length > 0){
+                for(var x =0; x < cardDetails.co_owner_ids.length; x++){
+                    const coOwnerObject = users.find(function(item, i){
+                        if(item.user_id === cardDetails.co_owner_ids[x]){
+                          index = i;
+                          return i;
+                        }
+                    });
+                    co_owner__username.push(coOwnerObject.username);
+                    co_owner__avatar.push(coOwnerObject.avatar);
+                }
+                cardDetails.co_owner_usernames = co_owner__username;
+                cardDetails.co_owner_avatars = co_owner__avatar;
+            }
             res.json(cardDetails);
         }
         else {
