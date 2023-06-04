7cd4feb2428d4a799885f71303e9f26ead96dd8f
George Esc <georgeeturnere@gmail.com>
2023-06-04 02:29:23 -0600

    Restructure activity card elements

--- web-interface/components/ActivityCard.tsx
+++ web-interface/components/ActivityCard.tsx
@@ -1,8 +1,5 @@
 import actCard from '../styles/Activitycard.module.css';
 import adjustColor from '../helpers/lightenColor';
-import { Console } from 'console';
-import dynamic from 'next/dynamic';
-import { useState } from 'react';
 
 export type ActivityCardProps = {
     "card_id": number,
@@ -49,12 +46,15 @@ const ActivityCard = ({card_id, color, owner_avatar, title, owner_username, co_o
         <div>
             
             <div className={actCard.boardCard} style={{backgroundColor:boardCardColor}} onClick={()=> handleClick()}>
-                <div className={actCard.lane} style={{ backgroundColor: `#${lane_color}` }}>{lane_name}</div>
-                <div className={actCard.innerContainer}>
-                    <div className={actCard.text}>{title}</div>
-                    <div className={actCard.imageSection}>
-                        {(owner_avatar !=  null &&  owner_avatar!="") ? <img src={owner_avatar} alt="" className={actCard.photo}/> : <div className={actCard.wrap}><div className={nonPhoto} style={{background:letterBackground}}> <div className={actCard.letter}>{letter}</div> </div></div>}
+                
+                <div className={actCard.container}>
+                    <div className={actCard.lane} style={{ backgroundColor: `#${lane_color}` }}>{lane_name}</div>
+                    <div className={actCard.innerContainer}>
+                        <div className={actCard.text}>{title}</div>
                     </div>
+                </div>
+                <div className={actCard.imageSection}>
+                        {(owner_avatar !=  null &&  owner_avatar!="") ? <img src={owner_avatar} alt="" className={actCard.photo}/> : <div className={actCard.wrap}><div className={nonPhoto} style={{background:letterBackground}}> <div className={actCard.letter}>{letter}</div> </div></div>}
                 </div> 
             </div>
             <footer className={actCard.bottom} style={{backgroundColor:newColor}}></footer>
