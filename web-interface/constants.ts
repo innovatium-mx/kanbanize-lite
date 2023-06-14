da10cf5a67dca1c7866ebea4a50e158bb4705d2c
innovatium-mx <135089038+innovatium-mx@users.noreply.github.com>
2023-06-14 12:35:15 -0600

    URLCloud updated

--- web-interface/constants.ts
+++ web-interface/constants.ts
@@ -1,5 +1,5 @@
-export const urlCloud : string = 'https://fs96h11zh9.execute-api.us-east-1.amazonaws.com/';
+export const urlCloud : string = 'https://ycgicviwwi.execute-api.us-east-2.amazonaws.com/';
 export const urlLocal : string = 'http://localhost:4000/';
-export const getCommentsEndpoint : string = 'https://fs96h11zh9.execute-api.us-east-1.amazonaws.com/comments/';
+export const getCommentsEndpoint : string = 'https://ycgicviwwi.execute-api.us-east-2.amazonaws.com/comments/';
 export const cookieDomain = 'localhost'; 
 //export const cookieDomain = 'main.d3talfomr9y7xi.amplifyapp.com';
